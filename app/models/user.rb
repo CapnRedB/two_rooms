afac87083d98f3d588d70009a935f0984ca2c7f7
Joe Flynn <joflynn@gmail.com>
2015-07-12 19:49:18 -0400

    typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,23 +8,22 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable
 
-
-  validates_format_of :email, :without => PLACEHOLDER_REGEX, on: :update
-
   def self.find_for_oauth(auth, signed_in_resource = nil)
     identity = Identity.find_for_oauth(auth)
 
     user = signed_in_resource ? signed_in_resource : identity.user
     
+    p auth
     if user.nil?
       email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+
       email = auth.info.email if email_is_verified
       user = User.where(email: email).first if email
 
       if user.nil?
         user = User.new(
           name: auth.extra.raw_info.name,
-          email: email ? email : "#{PLACEHOLDER}-#{auth.id}-#{auth.provider}.com",
+          email: email,
           password: Devise.friendly_token[0, 20]
         )
         user.skip_confirmation! if user.respond_to?(:skip_confirmation)
@@ -33,14 +32,14 @@ class User < ActiveRecord::Base
     end
 
     if identity.user != user
-      identiy.user = user
-      identiy.save!
+      identity.user = user
+      identity.save!
     end
     user
   end
 
-  def email_verified?
-    self.email && self.email !~ PLACEHOLDER_REGEX
-  end
+  # def email_verified?
+  #   self.email && self.email !~ PLACEHOLDER_REGEX
+  # end
 
 end
