d309d51afd3097929a80c55ff22a7160cc670562
Joe Flynn <joflynn@gmail.com>
2015-07-14 14:45:57 -0400

    install and bootstrap ember

--- Gemfile
+++ Gemfile
@@ -46,6 +46,10 @@ gem 'omniauth-google'
 #gem 'omniauth-facebook'
 
 
+gem 'ember-rails'
+gem 'ember-source'
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require jquery
+//= require jquery_ujs
+//= require ember
+//= require ember-data
+//= require active-model-adapter
+//= require_self
+//= require ./two_rooms
+
+// for more details see: http://emberjs.com/guides/application/
+TwoRooms = Ember.Application.create();
+
 //= require_tree .
